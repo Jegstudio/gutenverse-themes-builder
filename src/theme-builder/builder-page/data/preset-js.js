b26000ebfc8925f2a94348c1ba8dd8d48a6f0272
Yoga <yoga@jegstudio.com>
2023-08-07 10:23:43 +0800

    Fix asset preset content

--- src/theme-builder/builder-page/data/preset-js.js
+++ src/theme-builder/builder-page/data/preset-js.js
@@ -2,14 +2,19 @@ export const PRESET_JS = `"use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
     function playAnimation(element, frame = false) {
+        if (frame) {
+            element.style.visibility = 'visible';
+            element.style.opacity = '1';
+            element.style.transform = 'none';
+        }
         if (element.getBoundingClientRect().top > 0 && element.getBoundingClientRect().top <= (window.innerHeight * 0.75)) {
-            element.classList.add('{{theme_slug}}-animate-init');
+            element.classList.add('{{theme_slug}}-animate-init');            
         }
     }
 
     function prepareAnimation(doc, frame = false) {
         var elements = doc.getElementsByClassName('{{theme_slug}}-animate');
-    
+
         for (let element of elements) {
             if ( frame ) {
                 playAnimation(element, true);
@@ -25,4 +30,10 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     prepareAnimation(document);
+
+    setTimeout(function() {
+        var iframe = document.getElementsByClassName('edit-site-visual-editor__editor-canvas');
+        var innerDoc = iframe.length > 0 ? iframe[0].contentDocument || iframe[0].contentWindow.document : null;
+        innerDoc ? prepareAnimation(innerDoc, true) : null;
+    }, 3000);
 });`;
