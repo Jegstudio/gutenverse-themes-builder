65cac60dc96c5eb9c5ca8c0c957d6232fe84e6bd
Yoga <yoga@jegstudio.com>
2024-08-21 13:54:23 +0800

    [release/1.0.1] update version

--- gutenverse-themes-builder/readme.txt
+++ gutenverse-themes-builder/readme.txt
@@ -3,7 +3,7 @@ Contributors: Jegstudio
 Tags: themes, builder, template, block, editor
 Tested up to: 6.6
 Requires PHP: 7.0
-Stable tag: 1.0.0
+Stable tag: 1.0.1
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 
@@ -70,6 +70,9 @@ For additional information and assistance, please visit our extensive <a href="h
 
 
 == Changelog ==
+
+= 1.0.1 =
+* Add new themes dashboard builder
 
 = 1.0.0 - 2024-08-17 =
 * Initial Release
--- gutenverse-themes-builder/themes-builder.php
+++ gutenverse-themes-builder/themes-builder.php
@@ -4,7 +4,7 @@
  * Description: Build advance themes that fit for wp.org and any other marketplace.
  * Plugin URI: https://gutenverse.com/
  * Author: Jegstudio
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author URI: https://jegtheme.com/
  * License: GPLv3
  * Text Domain: gutenverse-themes-builder
@@ -13,7 +13,7 @@
  */
 
 defined( 'GUTENVERSE_THEMES_BUILDER' ) || define( 'GUTENVERSE_THEMES_BUILDER', 'gutenverse-themes-builder' );
-defined( 'GUTENVERSE_THEMES_BUILDER_VERSION' ) || define( 'GUTENVERSE_THEMES_BUILDER_VERSION', '1.0.0' );
+defined( 'GUTENVERSE_THEMES_BUILDER_VERSION' ) || define( 'GUTENVERSE_THEMES_BUILDER_VERSION', '1.0.1' );
 defined( 'GUTENVERSE_THEMES_BUILDER_NOTICE_VERSION' ) || define( 'GUTENVERSE_THEMES_BUILDER_NOTICE_VERSION', '1.0.0' );
 defined( 'GUTENVERSE_THEMES_BUILDER_NAME' ) || define( 'GUTENVERSE_THEMES_BUILDER_NAME', 'Gutenverse Themes Builder' );
 defined( 'GUTENVERSE_THEMES_BUILDER_URL' ) || define( 'GUTENVERSE_THEMES_BUILDER_URL', plugins_url( GUTENVERSE_THEMES_BUILDER ) );
